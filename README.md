47e534f5b4772f99cc4bd1834e5e925672dcfa6f
sidneyevpereira <sidneyevpereira@gmail.com>
2022-03-24 10:48:12 -0300

    Ajustado comentários nos códigos

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+No docker executar os seguintes comandos abaixo
+
+sudo apt update
+
+sudo apt install git
+
+git clone git@github.com:sidneyevpereira/unifique.gi
+
+cd unifique
+
+docker-compose create
+
+docker-compose start
+
+# Pagina Helo Word
+http://<endereco-url-servidor>:80
+
+# Pagina Phpmyadmin
+http://<endereco-url-servidor>:8080
+
 
--- php-files/conexao.php
+++ php-files/conexao.php
@@ -2,13 +2,8 @@
       $usuario_mysql="root";
       $senha_mysql="unifique@2022xxx"; 
       $conteiner_name="cn-mariadb";
-      $nome_banco="unifique_db";       
-
-      $usuario_mysql="root";
-      $senha_mysql="unifique@2022xxx"; 
-      $conteiner_name="cn-mariadb";
-      $nome_banco="unifique_db";        
-  
+      $nome_banco="unifique_db";   
+       
       $con = mysqli_connect($conteiner_name,$usuario_mysql,$senha_mysql,$nome_banco);    
       mysqli_select_db($nome_banco, $con);
   ?>
